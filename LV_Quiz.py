95e245cec8703aba54662be754645b9c14183314
Les Pounder <lespounder@gmail.com>
2014-06-07 20:37:10 +0100

    Added music

--- LV_Quiz.py
+++ LV_Quiz.py
@@ -1,9 +1,28 @@
+#!/usr/bin/python
+
 from easygui import *
 import time
+import pygame
+
+pygame.init()
+pygame.mixer.init()
+
+def intro():
+    intro=pygame.mixer.music.load('intro.mp3')
+    pygame.mixer.music.play(1)
+
+def win():
+    win=pygame.mixer.music.load('correct.mp3')
+    pygame.mixer.music.play(1)
+
+def lose():
+    lose=pygame.mixer.music.load('wrong.mp3')
+    pygame.mixer.music.play(1)
 
 score = 0
 logo = "./images/masthead.gif"
 play = ["Yes","No"]
+intro()
 game_start = buttonbox(title="Welcome to Linux Voice Python Quiz",image=logo,msg="Play the Quiz?",choices=play)
 
 print(game_start)
@@ -18,11 +37,13 @@ if game_start != "No":
         q1choices = ["Integer","Float"]
         q1 = choicebox(msg,title,q1choices)
         if q1 == "Float":
+            win()
             score = score + 1
             msgbox(title="CORRECT",image="./images/tick.gif",msg="Well done you got it right. Your score is "+str(score))
             break
         else:
-            msgbox(title="Wrong Answer",image="./images/cross.gif",msg="Oh dear, try again")
+            lose()
+            msgbox(title="Wrong Answer",image="./images/cross.gif",msg="Oh dear, try again")    
 
     #Question 2
     while True:
@@ -31,11 +52,14 @@ if game_start != "No":
         q2choices = ["Equal to","Not Equal To"]
         q2 = choicebox(msg,title,q2choices)
         if q2 == "Not Equal To":
+            win()
             score = score + 1
             msgbox(title="CORRECT",image="./images/tick.gif",msg="Well done you got it right. Your score is "+str(score))
             break
         else:
+            lose()
             msgbox(title="Wrong Answer",image="./images/cross.gif",msg="Oh dear, try again")
+            
 
     #Question 3
     while True:
@@ -44,10 +68,14 @@ if game_start != "No":
         q3choices = ["x == 10","x = 10","x = str(10)"]
         q3 = choicebox(msg,title,q3choices)
         if q3 == "x = 10":
+            win()
             score = score + 1
             msgbox(title="CORRECT",image="./images/tick.gif",msg="Well done you got it right. Your score is "+str(score))
             break
         else:
+            lose()
             msgbox(title="Wrong Answer",image="./images/cross.gif",msg="Oh dear, try again")
+
 gameover = "./images/well_done.gif"
+intro()
 game_over = msgbox(title="Linux Voice Python Quiz",image=gameover,msg="Well done you scored "+str(score))
